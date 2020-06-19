5b99b8260dadaed8ec7117957b00c01cc43dfd26
Hoang Nguyen <hoang_hoang5309@hotmail.com>
2020-06-19 13:44:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sal
+ RPG game where you can choose whoever you want to become. 0 to Hero!
---
+++ Sal/Assets/Codes/Login/login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class login : MonoBehaviour
+{
+    public string url = "http://foryoutiamo.com/libertas/login.php";
+    public Button login_btn;
+    public InputField username_input;
+    public InputField password_input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        login_btn.onClick.AddListener(submit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void submit()
+    {
+        StartCoroutine(connection());
+    }
+
+    IEnumerator connection()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", username_input.text);
+        form.AddField("pass", password_input.text);
+
+        UnityWebRequest con = UnityWebRequest.Post(url, form);
+        //WWW con = new WWW(url, form, postHeader);
+
+        yield return con.SendWebRequest();
+
+        if (Int32.Parse(con.downloadHandler.text) == 1)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
